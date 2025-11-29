fe8442aa4fbfeed5dd3166f15d007795f2d6fc23
Artem Klinkov <art.klinkov@gmail.com>
2025-11-29 10:18:43 +0000

    KS-5-8 вынести урл в константу, использовать forEach

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,15 @@ package org.example
 
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
-fun main() {
-    val doc: Document = Jsoup.connect("https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/").get()
+const val URL = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
 
-    val quotations: Elements = doc.select(".sc-2aegk7-2")
-    for (quotation: Element in quotations) {
-        println(quotation.text())
+fun main() {
+    val doc: Document = Jsoup.connect(URL).get()
+
+    val quotations: Elements = doc.select("article")
+    quotations.forEach {
+        println(it.text())
     }
 }
